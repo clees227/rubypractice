ac7de2f190f857e8dd461e4741b81fe728723bcb
clees227 <mpamff22@gmail.com>
2014-08-04 16:51:24 -0400

    Lychrel Numbers unfinished

---
+++ ProjectEuler/lychrelNumbers.rb
@@ -0,0 +1,23 @@
+class String
+	def palindrome?()
+		self == self.reverse
+	end
+end
+
+class Fixnum
+	def lychrel?()
+		tot = self + self.to_s.reverse.to_i
+		tot.to_s.palindrome?
+	end
+end
+
+startTime = Time.now
+num = 0
+1.upto(10000).each do |i|
+	if i.lychrel?
+		num += 1
+	end
+end
+endTime = Time.now
+puts "Answer: #{num}"
+puts "Start Time: #{startTime}\nEnd Time: #{endTime}\nTotal Time: #{endTime-startTime}"
